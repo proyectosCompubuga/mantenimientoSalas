0312d6fa361733e991fab188132ed651a1338e87
juan alegrias <juan.alegrias.22@gmail.com>
2020-07-15 17:14:44 -0500

    In-001:Se realizo segundo cambio.

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
     header('Location: seguridad/loginVista.php');
+    $numero =587383;
+
+    if ($numero % 2 ==0) {
+        echo 'el numero' .$numero.' es par';
+
+    }else{
+        echo 'el numero' .$numero.' no es par';
+    }
+    
     
 ?>
+
+
