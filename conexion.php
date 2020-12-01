780e46eb74c30c467236f6796f9ee99319d40d62
juan142 <juansrb132@gmail.com>
2020-12-01 11:33:04 -0500

    cambios en la conexion.

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("mysql5016.site4now.net", "a68046_proyec", "cb1006jsal");
-mysqli_select_db($link, "db_a68046_proyec");
+$link = mysqli_connect("mysql5035.site4now.net", "a6b655_laborat", "Cb1006jsal");
+mysqli_select_db($link, "db_a6b655_laborat");
 $tildes = $link->query("SET NAMES 'utf8'"); //Para que se muestren las tildes correctamente
 
